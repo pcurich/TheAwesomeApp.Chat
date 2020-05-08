3cb0846784a3c4a992933950c0819f762e63b1ce
Pedro Curich <curichpedro@gmail.com>
2020-05-08 13:00:48 -0500

    add storyboard

---
+++ ChatModule/ChatModule.h
@@ -0,0 +1,19 @@
+//
+//  ChatModule.h
+//  ChatModule
+//
+//  Created by QUBICGO SAC on 5/8/20.
+//  Copyright © 2020 Qubicgo SAC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChatModule.
+FOUNDATION_EXPORT double ChatModuleVersionNumber;
+
+//! Project version string for ChatModule.
+FOUNDATION_EXPORT const unsigned char ChatModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChatModule/PublicHeader.h>
+
+
---
+++ ChatModule/ChatViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChatModuleController.swift
+//  ChatModule
+//
+//  Created by QUBICGO SAC on 5/8/20.
+//  Copyright © 2020 Qubicgo SAC. All rights reserved.
+//
+
+import UIKit
+
+class ChatModuleController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
